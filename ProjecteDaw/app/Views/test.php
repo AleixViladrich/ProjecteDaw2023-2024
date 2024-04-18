c0e5e0af346d4001c69a0c5288b9d8fcddd78184
adriahenares <adriahenares@gmail.com>
2024-04-18 20:21:42 +0200

    Doing Datatables

--- ProjecteDaw/app/Views/test.php
+++ ProjecteDaw/app/Views/test.php
@@ -1,9 +1,16 @@
-<?= $this->extend('layouts/default/default'); ?>
+<?= $this->extend('layouts/mainLayout'); ?>
 <?= $this->section("main_content"); ?>
 <script>
+  import DataTable from 'datatables.net-dt';
   const dataSet = <?= json_encode($data) ?>;
   console.log(dataSet);
-  const table = new DataTable('#tickets');
+  const table = new DataTable('#tickets', {
+    columns: [
+      { title: 'Ticket_ID' },
+      { title: 'device_type_id' }
+    ],
+    data: dataSet
+  });
 </script>
 <table id="tickets" class="display" width="100%"></table>
 <?= $this->endSection(); ?>
